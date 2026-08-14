61cfba98ac5f82db092de62d7caa40a3d2c64ff4
Abrahamvazquez19 <147737860+Abrahamvazquez19@users.noreply.github.com>
2026-08-14 15:37:44 -0600

    Refactor app.py to improve code structure

--- app.py
+++ app.py
@@ -209,6 +209,8 @@ def crear_mesero():
     return jsonify({"status": "success", "nombre": nombre_mesero})
 
 
+# INICIALIZACIÓN AUTOMÁTICA (Para la Nube / Gunicorn)
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     app.run(host="0.0.0.0", port=5000, debug=True)
